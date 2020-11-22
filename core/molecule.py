22c091e2a06bf4d4e5a60dc5b8de0486faa9e6dc
Reno Raksi <adriano.raksi@gmail.com>
2020-11-22 14:41:31 -0600

    Implemented Molecule.complete_formula()

--- core/molecule.py
+++ core/molecule.py
@@ -1,6 +1,8 @@
 import collections
 from typing import Union, List
 
+from core.util import tokenize
+
 
 def _parse(toks: List[str]):
     op_stack: List[str] = []
@@ -48,24 +50,47 @@ def _parse(toks: List[str]):
     return result
 
 
-from core import tokenize
-
-
-print(_parse(tokenize('AliefOfc3')))
-
-
 class Molecule:
-    __slots__ = ('_formula', '_elements', '_charge', '_states')
-
     def __init__(self, formula_toks: List[str], charge: Union[str, int] = 0, states: List = None):
         self._formula = ''.join(formula_toks)
         self._elements = collections.Counter(_parse(formula_toks))
-        self._charge = charge
-        self._states = states if not states else []
+        self._charge = int(charge)
+        self._states = states if states else []
+        self._charge_sign = '+' if self._charge >= 0 else '-'
+        self._charge_mag = abs(self._charge)
 
     @staticmethod
     def complete_formula(complete_formula: str):
-        pass
+        toks: List[str] = tokenize(complete_formula)
+
+        try:
+            charge_start = toks.index('+')
+        except ValueError:
+            try:
+                charge_start = toks.index('-')
+            except ValueError:
+                charge_start = 0  # charge cannot be first character
+
+        if charge_start > 0:
+            # sign, number, ... OR sign, ...
+            try:
+                charge_is_one = not toks[charge_start + 1].isnumeric()
+            except IndexError:
+                charge_is_one = True
+            states_start = charge_start + (1 if charge_is_one else 2)
+            charge = ''.join(toks[charge_start:states_start] + (['1'] if charge_is_one else []))
+        else:
+            charge = 0
+            try:
+                states_start = toks.index('[')
+            except ValueError:
+                states_start = 0  # states cannot be first character
+
+        # states must take up the rest of the list
+        states = toks[states_start + 1:len(toks) - 1] if states_start else None
+        formula_toks = toks[:charge_start] if charge_start else toks[:states_start] if states_start else toks
+
+        return Molecule(formula_toks, charge, states)
 
     @property
     def formula(self):
@@ -81,4 +106,21 @@ class Molecule:
 
     @property
     def states(self):
-        return self._states
+        return self._states.copy()
+
+    @property
+    def charge_sign(self):
+        return self._charge_sign
+
+    @property
+    def charge_mag(self):
+        return self._charge_mag
+
+    def __str__(self):
+        return self._formula + \
+               (self._charge_sign + (str(self._charge_mag) if self._charge_mag != 1 else '')
+                if self._charge_mag != 0 else '') + \
+               ('[' + ', '.join(state for state in self._states) + ']' if self._states else '')
+
+    def __repr__(self):
+        return '(' + self._formula + ',' + self._charge_sign + str(self._charge_mag) + ',' + str(self._states) + ')'
